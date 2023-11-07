4cc3cddbde84bb8c11a1d8512132aefa6e8ff193
JuanDiSIlva <silvachaves@unicauca.edu.co>
2023-11-07 18:38:21 -0500

    FuncionEscucharEvento

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const resultado = document.querySelector('#resultado');
 
+//Para escuchar el evento y capturar el click
+btn.addEventListener('click', escucharBtnClick);
+
+
 function escucharBtnClick()
 {
   const sumaInputs = parseInt(input1.value) + parseInt(input2.value); 
