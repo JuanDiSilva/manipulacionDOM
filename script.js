2eacd133d61f6629b789963bd0572cde8dac76c0
JuanDiSIlva <silvachaves@unicauca.edu.co>
2023-11-07 18:01:16 -0500

    creacion botonesV2

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafo = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const resultado = document.querySelector('#resultado');
 
-console.log(input.value);
-
-console.log({
-    h1,
-    parrafo,
-    pid,
-    input,
-});
-
-h1.innerHTML = "JULIO <br> julio";
-//h1.innerText ="HOLA MUNDO";
-
-//Clases
-//console.log(h1.getAttribute('class','rojo'));
-h1.classList.add('rojo');
-h1.classList.remove('juliopc');
-//h1.classList.toggle('juliopc');
-
-input.value = "456";
-
-//Crea una etiqueta en html y se inserta una imagen
-const img = document.createElement('img');
-img.setAttribute('src', 'https://www.gran-turismo.com/gtsport/decal/5980859549996811784_1.png');
-pid.innerHTML = "";
-pid.appendChild(img);
+function escucharBtnClick()
+{
+  const sumaInputs = parseInt(input1.value) + parseInt(input2.value); 
+  resultado.innerText  = "Resulatado: " + sumaInputs;
+}
