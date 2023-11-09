46bc1b6e7b506f750229968aa078b14abb4fac0e
JuanDiSIlva <silvachaves@unicauca.edu.co>
2023-11-09 15:11:14 -0500

    Asignacion .gitignore

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const h1 = document.querySelector('h1');
+const forma = document.querySelector('#formulario');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const resultado = document.querySelector('#resultado');
 
 //Para escuchar el evento y capturar el click
-btn.addEventListener('click', escucharBtnClick);
+forma.addEventListener('submit', sumarImputs);
 
 
-function escucharBtnClick()
+function sumarImputs(event)
 {
+  console.log({event});
+  //Por la etiqueta formulario hay que colocar un preventDefault, para que no se reinicie de inmediato
+  event.preventDefault();
   const sumaInputs = parseInt(input1.value) + parseInt(input2.value); 
   resultado.innerText  = "Resulatado: " + sumaInputs;
 }
